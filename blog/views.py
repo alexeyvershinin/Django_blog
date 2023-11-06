109300087b5f04801176352edf2d076232e0d0c6
aleksejversinin <og2001@yandex.ru>
2023-11-06 13:01:23 +0300

    added the ability to share a post by email

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
+from django.core.mail import send_mail
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from .forms import EmailPostForm
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage
 
 
 def post_list(request):
@@ -10,11 +12,41 @@ def post_list(request):
     page_number = request.GET.get('page', 1)
     try:
         posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Если page_number не целое число, то выдать первую страницу
+        posts = paginator.page(1)
     except EmptyPage:
+        # Если page_number находится вне диапазона, то выдать последнюю страницу результатов
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month,
+                             publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cleaned_data['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cleaned_data['name']}\'s comments: {cleaned_data['comments']}\n"\
+                      f"You can contact the author of the post by email: {cleaned_data['email']}"
+            send_mail(subject, message, 'your_account@gmail.com', [cleaned_data['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    context = {
+        'post': post,
+        'form': form,
+        'sent': sent
+    }
+    return render(request, 'blog/post/share.html', context)
